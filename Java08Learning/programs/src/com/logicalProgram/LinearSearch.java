3c3c984ba27465554e900e8aa3faef4e04f9c5c7
afzalmalik6432 <afzalmalik6432@gmail.com>
2022-10-10 15:59:22 +0530

    linearSearch

---
+++ Java08Learning/programs/src/com/logicalProgram/LinearSearch.java
@@ -0,0 +1,38 @@
+package com.logicalProgram;
+
+import java.util.Scanner;
+
+/**
+ * LinearSearch
+ */
+public class LinearSearch {
+
+    private static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if(target == arr[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array ");
+        int n = sc.nextInt();
+        int []arr = new int[n];
+        System.out.println("Enter the element: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter the target value: ");
+        int target = sc.nextInt();
+        int result = linearSearch(arr,target);
+        if(result>=0){
+            System.out.println("Element found at index: "+result);
+        }
+        else{
+            System.out.println("Element not found");
+        }
+        sc.close();
+    } 
+}

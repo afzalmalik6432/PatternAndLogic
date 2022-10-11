f339ad93a364cded7d40358971487a67bbbd4511
afzalmalik6432 <afzalmalik6432@gmail.com>
2022-10-11 13:14:58 +0530

    addOneToArray

---
+++ Java08Learning/programs/src/com/logicalProgram/AddOneToArray.java
@@ -0,0 +1,32 @@
+package com.logicalProgram;
+
+import java.util.Scanner;
+
+
+public class AddOneToArray {
+
+    public static int[] addOne(int[] arr){
+        for(int i=arr.length-1; i>=0; i--){
+            if(arr[i] < 9){
+                arr[i] += 1;
+                return arr;
+            }
+            arr[i] = 0;
+        }
+        int[] nums = new int[arr.length+1];
+        nums[0] = 1;
+        return nums;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int[] result = addOne(arr);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i]+" , ");
+        }
+    }
+}

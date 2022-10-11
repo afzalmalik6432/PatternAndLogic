d15aba631f212135d74c5d40b432bbc0df1a49ec
afzalmalik6432 <afzalmalik6432@gmail.com>
2022-10-11 13:46:48 +0530

    XpatternAdded

---
+++ Java08Learning/programs/src/com/patterns/Pattern45.java
@@ -0,0 +1,22 @@
+package com.patterns;
+
+import java.util.Scanner;
+
+public class Pattern45 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(i==j || j+i == n+1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}

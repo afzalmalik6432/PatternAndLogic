47e8d1b115bab879ad6c4fa71fd202eb09fac5ef
afzalmalik6432 <afzalmalik6432@gmail.com>
2022-10-14 00:17:54 +0530

    repeatedAndMissingNumber

--- Java08Learning/programs/src/com/logicalProgram/RepeatedAndMissing.java
+++ Java08Learning/programs/src/com/logicalProgram/RepeatedAndMissing.java
@@ -10,14 +10,24 @@ public class RepeatedAndMissing {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < n; i++) {
             int val = Math.abs(arr[i]);
-            if(arr[val-1]>val) arr[val-1] = arr[val-1];
-            else System.out.println(val);
+            if(arr[val-1]>0) 
+            {
+                arr[val-1] = -arr[val-1];
+            }
+            else
+            {
+                System.out.println(val);
+            }
         }
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i]>0) System.out.println(arr[i]);
+        for (int i = 0; i < n; i++) {
+            if(arr[i]>0) 
+            {
+                System.out.println(arr[i+1]);
+            }
         }
         sc.close();
     }
 }
+
